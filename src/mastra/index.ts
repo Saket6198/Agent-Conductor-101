429fc1a94c5ebf57218122a25a3f236f2cb071e8
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-07-03 17:11:34 +0530

    initial implementation of Mastra

---
+++ src/mastra/index.ts
@@ -0,0 +1,26 @@
+import { Mastra } from "@mastra/core/mastra";
+import { PinoLogger } from "@mastra/loggers";
+import { LibSQLStore } from "@mastra/libsql";
+import { weatherWorkflow } from "./workflows/weather-workflow";
+import { weatherAgent } from "./agents/weather-agent";
+import { financialAgent } from "./agents/financial-agent";
+import { personalAssistantAgent } from "./agents/personal-assistant";
+// import { personalAssistantAgent } from "./agents/personal-assistant";
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+  agents: {
+    weatherAgent,
+    // financialAgent,
+    personalAssistantAgent
+    // personalAssistant: await personalAssistantAgent, // Await needed to ensure MCP tools are loaded
+  },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: "Mastra",
+    level: "info",
+  }),
+});
